ed929b7172dcc2446edb5efd8900901b157645fe
Vladislav <robertsarychev9@gmail.com>
2025-12-10 20:20:49 +0300

    Почти целиком переправлено выделение памяти в системе управления контейнерами. Добавлены два режима работы. Данная версия содержит в себе тесты в том виде, в каком они были на момент сохранения. Сортировка временно отключена

--- GnomSorted.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "house.h"
-#include "housecont.h"
-
-HouseCont_t *GnomSortedCont(HouseCont_t *housecontP);
-int CompareCont(HouseContElem_t *fhousecontelem, HouseContElem_t *shousecontelem, unsigned int index);
